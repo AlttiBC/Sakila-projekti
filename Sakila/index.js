3b0e86b8dedc40c8193800bff2aab724984c9f6e
Altti Kolkki <s2100988@edu.bc.fi>
2023-05-10 10:16:32 +0300

    Elokuvat sivulle sivutus
    Elokuvat ladataan sivuittain, käyttäjä painaa sivun alhaalta ladatakseen lisää elokuvia. Jokaisella sivulla on 20 elokuvaa.

--- Sakila/index.js
+++ Sakila/index.js
@@ -3,9 +3,13 @@ const dbconfig = require('./config.json');
 const mariadb = require('mariadb');
 const path = require('path');
 const app = express();
+const paginate = require("express-paginate");
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(paginate.middleware(20, 20));
 
 app.set('view engine', 'ejs');
 
@@ -31,9 +35,12 @@ app.get('/', async (req, res) => {
 
 app.get('/elokuvat', async (req, res) => {
     const connection = await mariadb.createConnection(dbconfig);
-    
+
     let elokuvat;
-    if (req.query.category) {
+    const elokuvatmaara = await connection.query(`
+    SELECT COUNT(*) as count FROM film; 
+    `);
+    if(req.query.category) {
         elokuvat = await connection.query(`
         SELECT category.name AS category_name, title, description, release_year, rental_duration, rental_rate, rating 
         FROM film
@@ -42,8 +49,8 @@ app.get('/elokuvat', async (req, res) => {
         INNER JOIN category
         ON film_category.category_id = category.category_id
         WHERE film_category.category_id = ${req.query.category}
-        ORDER BY title
-        LIMIT 20;
+        OFFSET ${req.query.page * req.query.limit} 
+        LIMIT 20 ROWS;
         `);
     } else {
         elokuvat = await connection.query(`
@@ -53,14 +60,28 @@ app.get('/elokuvat', async (req, res) => {
         ON film.film_id = film_category.film_id  
         INNER JOIN category
         ON film_category.category_id = category.category_id
-        ORDER BY title
-        LIMIT 20;
+        LIMIT 20
+        OFFSET ${req.query.page * req.query.limit};
         `);
     }
     const kategoriat = await connection.query(`SELECT name, category_id FROM category;`)
+    const sivumaara = Number(elokuvatmaara[0].count) / 20;
 
-    res.render('elokuvat', { elokuvat, kategoriat });
+    res.render('elokuvat', { elokuvat, kategoriat, pages: paginate.getArrayPages(req)(1, sivumaara, req.query.page) });
     connection.end();
+});
+
+app.get('/näyttelijät', (req, res) => {
+    res.render('nayttelijat')
+});
+
+app.get('/kategoriat/:id', (req, res) => {
+    res.render('kategoriat')
+});
+
+app.all('*', (req, res) => {
+    res.status(404);
+    res.render('404');
 });
 
 const port = 3000;
